66a8ad78f86640acee37643d0f27b85c35fa9c7e
Abhishek kumar <110687729+abhiishek031@users.noreply.github.com>
2023-02-05 16:30:29 +0530

    modified

--- js/script.js
+++ js/script.js
@@ -73,23 +73,6 @@ var result = document.querySelector(".result");
 var statement = document.querySelector(".result-statement");
 var BMI, height, weight;
 
-calculateButton.addEventListener("click", ()=>{
-    
-    height = heightInput.value;
-    weight = weightInput.value;
-    BMI = weight/(height**2); 
-    result.innerText = BMI;
-
-    if(BMI < 18.5){
-        statement.innerText = "Your BMI falls within the underweight range";    
-    }else if((BMI > 18.5) && (BMI < 24.9)){
-        statement.innerText = "Your BMI falls within the normal or healthy weight range";
-    }else if((BMI > 25) && (BMI < 29.9 )){
-        statement.innerText = "Your BMI falls within the overweight range";
-    }else{
-        statement.innerText = "Your BMI falls within the obese range";
-    }
-});
 function Calculate(){
     var height = document.getElementById("h-input").value;
     var weight = document.getElementById("w-input").value;
@@ -112,4 +95,9 @@ function Calculate(){
         }
     }
   }
-  
+
+ document.addEventListener('keypress',function(e){
+    if(e.key==='Enter'){
+        Calculate();
+    }
+ });
